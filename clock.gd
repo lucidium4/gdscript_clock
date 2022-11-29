0659967922b9f66565d7e3344a186532606d313d
mintkat <85989407+m1ntkat@users.noreply.github.com>
2022-11-29 09:52:31 -0900

    Update clock.gd

--- clock.gd
+++ clock.gd
@@ -1,6 +1,6 @@
 extends Node
 
-func get_time():
+func get_12hour_time():
 	var datetime = OS.get_datetime()
 	var hour = datetime.hour
 	var minute = datetime.minute
@@ -9,4 +9,11 @@ func get_time():
 		ampm = " PM"
 		hour = hour - 12
 	var time = var2str(hour) + ":" + var2str(minute) + ampm
+	return time
+
+func get_24hour_time():
+	var datetime = OS.get_datetime()
+	var hour = datetime.hour
+	var minute = datetime.minute
+	var time = var2str(hour) + ":" + var2str(minute)
 	return time
